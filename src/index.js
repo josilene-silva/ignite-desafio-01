f68e22a04ff447598dbdbe4ad77376bb95d132d6
Josilene Vitoria dos Santos da Silva <josilenevitoriasilva@gmail.com>
2021-12-05 22:15:31 -0400

    implementa listagem de todos

--- src/index.js
+++ src/index.js
@@ -50,7 +50,8 @@ app.post('/users', (request, response) => {
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  return response.json(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
